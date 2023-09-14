14ddf82b3e60031f2d9e224591f2a3dc7cfa8fff
Miguel Ferreira <mferreira.oeiras@gmail.com>
2023-09-14 16:54:16 +0100

    Fixes incorrect divs closing

--- public/index.js
+++ public/index.js
@@ -1,12 +1,12 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 gsap.registerPlugin();
 
+// create the scrollSmoother before your scrollTriggers
 ScrollSmoother.create({
-  wrapper: '.smoothWrapper',
-  content: '.smoothContent',
-  smooth: 1.5,
-  effects: true
-})
+  smooth: 1,               // how long (in seconds) it takes to "catch up" to the native scroll position
+  effects: true,           // looks for data-speed and data-lag attributes on elements
+  smoothTouch: 0.1,        // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+});
 
 /*ScrollTrigger.normalizeScroll({
   allowNestedScroll: true, 
