0c54843ddf4b1471240923117126304aa95562fc
Miguel Ferreira <mferreira.oeiras@gmail.com>
2023-09-14 16:35:40 +0100

    Improves how we deal with momentum

--- public/index.js
+++ public/index.js
@@ -2,7 +2,12 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin();
 
 
-ScrollTrigger.normalizeScroll(true); // enable
+ScrollTrigger.normalizeScroll({
+  allowNestedScroll: true, 
+  lockAxis: false,
+  momentum: self => Math.min(3, Math.abs(self.velocityY) / 1000), // dynamically control the duration of the momentum when flick-scrolling
+  type: "touch,wheel,pointer", // now the page will be drag-scrollable on desktop because "pointer" is in the list 
+});
 
 function circles() {
   gsap.to("#secondCircle", {
